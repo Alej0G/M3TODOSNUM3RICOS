7a2777a93fb09e9067d7fb9ff9eb1732d98716e2
Samuel Restrepo <samuelrestreporua@gmail.com>
2026-05-27 22:20:08 -0500

    Detallers

---
+++ backend/methods/chapter1/__init__.py
@@ -0,0 +1,6 @@
+from .bisection import biseccion
+from .regla_falsa import regla_falsa
+from .punto_fijo import punto_fijo
+from .newton import newton
+from .secante import secante
+from .raices_multiples import raices_multiples
